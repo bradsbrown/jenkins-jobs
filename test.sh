6f5d8f6289329793737f0cbcc4a1a7697f6fe87b
Soren Hansen <Soren.Hansen@ril.com>
2014-12-15 11:34:01 +0100

    Use test.sh from the repo

--- test.sh
+++ test.sh
@@ -9,20 +9,6 @@ else
 fi
 
 export env
-
-. /var/lib/jenkins/cloud.${env}.env
-
-if [ -n "${fail_immediately}" ]
-then
-    exit 1
-fi
-
-rm -rf venv
-
-virtualenv venv
-. venv/bin/activate
-pip install -e git+https://github.com/JioCloud/python-jiocloud#egg=jiocloud
-
-python -m jiocloud.apply_resources ssh_config --project_tag=test${deploy_id} environment/cloud.${env}.yaml > ssh_config
-
-ssh -F ssh_config -l jenkins oc1_{env} '. <(sudo cat /root/openrc) ; glance image-list'
+export cloud_provider=hp
+chmod +x ./build_scripts/test.sh
+build_scripts/test.sh
